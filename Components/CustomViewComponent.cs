512de3c76c8f8d83738a8264916d56c0f34266b5
Dell-Simmons <dell.simmons@gmail.com>
2023-07-29 10:13:25 -0700

    Add BetterStripe

---
+++ Components/CustomViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace SIDS.Plugin.Payments.BetterStripe.Components
+{
+    [ViewComponent(Name = "Custom")]
+    public class CustomViewComponent : NopViewComponent
+    {
+        public CustomViewComponent()
+        {
+
+        }
+
+        public IViewComponentResult Invoke(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domains/CustomTable.cs
@@ -0,0 +1,9 @@
+﻿using Nop.Core;
+
+namespace SIDS.Plugin.Payments.BetterStripe.Domains
+{
+    public partial class CustomTable : BaseEntity
+    {
+
+    }
+}
